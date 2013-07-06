5738f967056583c5501d145edb4fe13d5bb2874d
cpmpercussion <cpm@charlesmartin.com.au>
2013-07-06 16:34:02 +1000

    Changed to fake cafe sounds, fixed up circle colours.

--- Metatone/MetatoneTouchView.m
+++ Metatone/MetatoneTouchView.m
@@ -13,6 +13,9 @@
 
 @property (strong, nonatomic) UIImage *lastFrame;
 @property (strong, nonatomic) CALayer *animationLayer;
+@property (strong, nonatomic) UIColor *touchColour;
+@property (strong, nonatomic) UIColor *loopColour;
+@property (strong, nonatomic) NSString *deviceID;
 
 @end
 
@@ -37,7 +40,32 @@
         self.opaque = NO;
         self.userInteractionEnabled = NO;
         
-        self.movingTouchCircleLayer = [self makeCircleLayerWithColour:[UIColor colorWithRed:1 green:1 blue:1 alpha:0.8]];
+        self.deviceID = [[UIDevice currentDevice].identifierForVendor UUIDString];
+        NSLog(self.deviceID);
+        // Processing Colour codes.
+        if ([self.deviceID isEqual:@"1D7BCDC1-5AAB-441B-9C92-C3F00B6FF930"]) {
+            //fill(224,23,26);
+            self.touchColour = [UIColor colorWithRed:0.9 green:0.1 blue:0.1 alpha:0.8];
+        } else if ([self.deviceID isEqual:@"6769FE40-5F64-455B-82D4-814E26986A99"]) {
+            //fill(23,27,224);
+            self.touchColour = [UIColor colorWithRed:0.1 green:0.1 blue:0.9 alpha:0.8];
+
+        } else if ([self.deviceID isEqual:@"2678456D-9AE7-4DCC-A561-688A4766C325"]) {
+            //fill(23,224,105);
+            self.touchColour = [UIColor colorWithRed:0.1 green:0.9 blue:0.4 alpha:0.8];
+
+        } else if ([self.deviceID isEqual:@"97F37307-2A95-4796-BAC9-935BF417AC42"]) {
+            //fill(224,204,23);
+            self.touchColour = [UIColor colorWithRed:0.9 green:0.8 blue:0.1 alpha:0.8];
+        } else {
+            //fill(255);
+            self.touchColour = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.8];
+        }
+        
+        self.loopColour = [UIColor colorWithRed:1 green:0 blue:0 alpha:0.8];
+        //
+        
+        self.movingTouchCircleLayer = [self makeCircleLayerWithColour:self.touchColour];
         [self.layer addSublayer:self.movingTouchCircleLayer];
         self.movingTouchCircleLayer.hidden = YES;
     }
@@ -49,24 +77,40 @@
 {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        self.movingTouchCircleLayer = [self makeCircleLayerWithColour:[UIColor colorWithRed:1 green:1 blue:1 alpha:0.8]];
+        
+        
+        self.deviceID = [[UIDevice currentDevice].identifierForVendor UUIDString];
+        NSLog(self.deviceID);
+        // Processing Colour codes.
+        if ([self.deviceID isEqual:@"1D7BCDC1-5AAB-441B-9C92-C3F00B6FF930"]) {
+            //fill(224,23,26);
+            self.touchColour = [UIColor colorWithRed:0.9 green:0.1 blue:0.1 alpha:0.8];
+        } else if ([self.deviceID isEqual:@"6769FE40-5F64-455B-82D4-814E26986A99"]) {
+            //fill(23,27,224);
+            self.touchColour = [UIColor colorWithRed:0.1 green:0.1 blue:0.9 alpha:0.8];
+        } else if ([self.deviceID isEqual:@"2678456D-9AE7-4DCC-A561-688A4766C325"]) {
+            //fill(23,224,105);
+            self.touchColour = [UIColor colorWithRed:0.1 green:0.9 blue:0.4 alpha:0.8];
+        } else if ([self.deviceID isEqual:@"97F37307-2A95-4796-BAC9-935BF417AC42"]) {
+            //fill(224,204,23);
+            self.touchColour = [UIColor colorWithRed:0.9 green:0.8 blue:0.1 alpha:0.8];
+        } else {
+            //fill(255);
+            self.touchColour = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.8];
+        }
+        self.loopColour = [UIColor colorWithRed:1 green:0 blue:0 alpha:0.8];
+        //
+
+        // init code.
+        self.movingTouchCircleLayer = [self makeCircleLayerWithColour:self.touchColour];
         [self.layer addSublayer:self.movingTouchCircleLayer];
         self.movingTouchCircleLayer.hidden = YES;
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-}
-*/
-
-
 -(void) drawTouchCircleAt:(CGPoint) point {
-    CALayer *layer = [self makeCircleLayerWithColour:[UIColor colorWithRed:1 green:1 blue:1 alpha:0.8]];
+    CALayer *layer = [self makeCircleLayerWithColour:self.touchColour];
     [self.touchCirclePoints addObject:layer];
     
     [CATransaction setAnimationDuration:0.0];
@@ -83,7 +127,7 @@
 
 
 -(void) drawNoteCircleAt:(CGPoint) point {
-    CALayer *layer = [self makeCircleLayerWithColour:[UIColor colorWithRed:1 green:0 blue:0 alpha:0.8]];
+    CALayer *layer = [self makeCircleLayerWithColour:self.loopColour];
     
     [self.noteCirclePoints addObject:layer];
     
@@ -97,9 +141,7 @@
             [self.noteCirclePoints removeObject:layer];
         }];
     }];
-    
 }
-
 
 -(CALayer *) makeCircleLayerWithColour:(UIColor *) colour {
     CALayer *layer = [[CALayer alloc] init];
@@ -109,8 +151,8 @@
     layer.shadowRadius = 5.0;
     layer.shadowColor = [UIColor blackColor].CGColor;
     layer.shadowOpacity = 0.8;
-    layer.frame = CGRectMake(0, 0, 30, 30);
-    layer.cornerRadius = 15.0;
+    layer.frame = CGRectMake(0, 0, 50, 50);
+    layer.cornerRadius = 25.0;
     [self.layer addSublayer:layer];
     layer.hidden = YES;
     
@@ -127,17 +169,5 @@
     [CATransaction setAnimationDuration:1.0];
     self.movingTouchCircleLayer.hidden = YES;
 }
-
-
-//-(CALayer *) movingTouchCircleLayer {
-//    if (!_movingTouchCircleLayer) {
-//        _movingTouchCircleLayer = [self makeCircleLayerWithColour:[UIColor colorWithRed:1 green:1 blue:1 alpha:0.8]];
-//    }
-//    [self.layer addSublayer:_movingTouchCircleLayer];
-//    _movingTouchCircleLayer.hidden = YES;
-//    return _movingTouchCircleLayer;
-//}
-
-
 
 @end
